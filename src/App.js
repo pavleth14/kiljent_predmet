b06c936aa52e637bc0aef055663ffcc5bda626db
PavleStojanovic <pavleth14@hotmail.com>
2024-02-08 15:19:41 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import SearchBar from './components/SearchBar';
 
 function App() {
+  const [clientList, setClientList] = useState([]);
+  const [name, setName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [currentCourtCode, setCurrentCourtCode] = useState('');
+  const [subject, setSubject] = useState('');  
+
+  useEffect(() => {
+      clientList.length > 0 && console.log(clientList);
+  }, [clientList]);  
+
+  const currentCourtCodeHandler = (e) => {
+    setCurrentCourtCode(e.target.value);
+  }
+
+  const nameHandler = (e) => {
+    setName(e.target.value);
+  }
+
+  const lastNameHandler = (e) => {
+    setLastName(e.target.value);
+  }
+
+  const subjectHandler = (e) => {
+    setSubject(e.target.value);
+  }
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    checkIsClientExistBasedOnCourtCode();
+  }
+
+  const checkIsClientExistBasedOnCourtCode = () => {
+
+      const isMatch = clientList.some(client => client.currentCourtCode === currentCourtCode);
+
+      console.log(isMatch);
+
+      if (isMatch) {
+
+        const updatedSubjects = clientList.map(client => {
+            if (client.currentCourtCode === currentCourtCode) {
+                // Add the new subject to the client's existing subjects
+                return { ...client, subject: [client.subject, subject] };
+            }
+            return client;
+        });
+
+        // Update the isClient state with the modified data
+        setClientList(updatedSubjects);
+        return;
+      }
+
+      setClientList(prevState => [...prevState, { name: name, lastName: lastName, currentCourtCode: currentCourtCode, subject: subject }]);
+      
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchBar clientList={clientList} />
+      <form onSubmit={handleFormSubmit}>
+        <label>Ime:
+          <input onChange={nameHandler} type="text" value={name} />
+        </label>
+        <label>Prezime:
+          <input onChange={lastNameHandler} type="text" value={lastName} />
+        </label>
+        <label>Sudska sifra:
+          <input onChange={currentCourtCodeHandler} type="text" />
+        </label>
+        <label>Predmet:
+          <input onChange={subjectHandler} type="text" />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
---
+++ src/components/SearchBar.js
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+
+const SearchBar = ({clientList}) => {
+
+    if(clientList.length > 0) {
+        console.log('SearchBar clientList: ', clientList);
+    }
+
+    const [inputValue, setInputValue] = useState('');
+    const [filteredArr, setFilteredArr] = useState([]);
+
+    useEffect(() => {
+        console.log('filteredArr: ',filteredArr);
+    }, [filteredArr])
+
+    const handleButton = (e) => {
+        e.preventDefault();
+        findClientByName();
+    }
+
+    const inputHandler = (e) => {
+        setInputValue(e.target.value);
+    }
+
+    const findClientByName = () => {
+        const isMatch = clientList.some(client => client.name === inputValue);
+        console.log(isMatch); //true
+
+        if(isMatch === false) {
+            console.log('Match is false');
+            setFilteredArr([]);
+        }
+
+        clientList.map(client => {
+            if(client.name === inputValue) {
+                setFilteredArr(prevState => [...prevState, client]);
+            }
+        })
+
+    }
+    
+
+    return ( 
+        <div>            
+            <input onChange={inputHandler} placeholder="Pronadji klijenta..." />
+            <button onClick={handleButton}>Click me</button>
+        </div>
+     );
+}
+ 
+export default SearchBar;
